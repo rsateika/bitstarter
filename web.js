b57cfb117dc5884c33eb5253bef93209c9a3b795
Randy Sateika <rsateika@gmail.com>
2013-07-09 16:39:23 +0000

    Changes

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var buff = new buffer(256);
 var infile = "index.html";
 
-buff = fs.readFileSync(infile);
+//buff = fs.readFileSync(infile);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
